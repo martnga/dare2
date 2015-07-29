a27d242bc91d4eeeec0dc2591a45eb8669aba5f2
mtnjoroge <mtnjoroge@gmail.com>
2015-07-29 20:54:31 +0300

    Intro extends AppIntro && populated init class

--- app/src/main/java/com/nganga/dare2/Intro.java
+++ app/src/main/java/com/nganga/dare2/Intro.java
@@ -1,7 +1,39 @@
 package com.nganga.dare2;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.github.paolorotolo.appintro.AppIntro;
+import com.github.paolorotolo.appintro.AppIntroFragment;
+
 /**
  * Created by nganga on 7/29/15.
  */
-public class Intro {
+public class Intro extends AppIntro {
+    @Override
+    public void init(Bundle savedInstanceState) {
+        addSlide(AppIntroFragment.newInstance("Hello!", "Kenya", image, R.color.hint_foreground_material_light));
+
+    }
+
+    private void loadMainActivity(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onSkipPressed() {
+        loadMainActivity();
+
+    }
+
+    @Override
+    public void onDonePressed() {
+        loadMainActivity();
+    }
+
+    public void getStarted(View v){
+        loadMainActivity();
+    }
 }
